c5f8abf5b07307533fe60d1f6c4c26451bd4a191
Kriti Srivastava <srivastavakriti324@gmail.com>
2026-08-09 01:23:21 +0530

    Added Interactive Sidebar

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById("menuButton");
+const sidebar = document.getElementById("sidebar");
+
+menuButton.addEventListener("click", fucntion() {
+    sidebar.classList.toggle("hidden");
+});
