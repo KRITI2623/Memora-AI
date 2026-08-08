8158db0e047d3e8c81dc93fa0db57eccee58d22b
Kriti Srivastava <srivastavakriti324@gmail.com>
2026-08-09 01:48:45 +0530

    Added dynamic folder creation

--- frontend/script.js
+++ frontend/script.js
@@ -4,3 +4,14 @@ const sidebar = document.getElementById("sidebar");
 menuButton.addEventListener("click", function () {
     sidebar.classList.toggle("hidden");
 });
+const newFolderButton = document.getElementById("newFolderButton");
+const folderList = document.getElementById("folderList");
+
+newFolderButton.addEventListener("click", function () {
+    const folderName = prompt("Enter Folder Name :");
+    if (folderName) {
+        const newFolder = document.createElement("li");
+        newFolder.textContent = "📁 " + folderName;
+        folderList.appendChild(newFolder);
+    }
+});
